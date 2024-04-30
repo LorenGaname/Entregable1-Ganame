7735573d5d96d3f45e8a77668b8842f3c6ccfe82
Loren <lorenzoganame@gmail.com>
2024-04-30 18:05:58 -0300

    corrección de errores

--- java.js
+++ java.js
@@ -1,20 +1,20 @@
 // Nombre usuario
-var nombre = prompt("Por favor, ingresa tu nombre:");
+let nombre = prompt("Por favor, ingresa tu nombre:");
 
 // Edad usuario
-var edad;
+let edad;
 do { 
     edad = prompt("Hola " + nombre + ", ¿cuál es tu edad? (Ingrese solo numeros)")
 }while (isNaN(edad));
 
 // Genero musical favorito
-var generoMusical = prompt("¿Cuál es tu género de música favorito?\nEscribe el número correspondiente:\n1. Rock\n2. Pop\n3. Hip Hop\n4. Jazz\n5. Electrónica");
+let generoMusical = prompt("¿Cuál es tu género de música favorito?\nEscribe el número correspondiente:\n1. Rock\n2. Pop\n3. Hip Hop\n4. Jazz\n5. Electrónica");
 
 // Convertir la entrada del género musical a un valor numérico
 generoMusical = parseInt(generoMusical);
 
 // Validar la selección del usuario y asignar el género correspondiente
-var genero;
+let genero;
 switch (generoMusical) {
     case 1:
         genero = "Rock";
@@ -36,7 +36,7 @@ switch (generoMusical) {
 }
 
 // Variable con la info del usuario
-var informacionUsuario = {
+let informacionUsuario = {
     nombre: nombre,
     edad: parseInt(edad),
     generoMusical: genero
